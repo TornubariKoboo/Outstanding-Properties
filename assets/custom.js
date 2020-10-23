fdb54e25b844f56e076f9ca0bdede304cd41ca8c
Tornubari <t.koboo12@gmail.com>
2020-10-23 01:31:58 -0400

    chagge content in search

--- assets/custom.js
+++ assets/custom.js
@@ -1,1 +1,5 @@
-console.log('love')
+$("#searchButton").click(function () {});
+
+$(".real-estate-pic").prependTo(
+  "< img src = https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThvo5oUPG8IJe1ySR8-Oygd36rbvycZNmxyQ&usqp=CAU)>"
+);
