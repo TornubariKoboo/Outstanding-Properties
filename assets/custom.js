4f12df40b80f7b36b3b3a9fc5e31d0ac12d16856
Tornubari <t.koboo12@gmail.com>
2020-10-23 13:35:48 -0400

    changed navbar text postion, backgorund and button postion

--- assets/custom.js
+++ assets/custom.js
@@ -1,5 +1,5 @@
-$("#searchButton").click(function () {});
+var realEstateImg =
+"https://swall.teahub.io/photos/small/1-17152_dream-house-wallpaper-house.jpg"
+setBgImg(realEstateImg);
 
-$(".real-estate-pic").prependTo(
-  "< img src = https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcThvo5oUPG8IJe1ySR8-Oygd36rbvycZNmxyQ&usqp=CAU)>"
-);
+$("#searchButton").click(function () {});
